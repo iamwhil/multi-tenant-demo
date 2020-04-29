3c8693bff9e01134ab83a8fab9af2924cf192a77
Whil Piavis <ncsupiratecaptain@gmail.com>
2020-04-29 14:19:30 -0600

    Now with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# This file grows small plants and feeds birds.
+org1 = Organization.create(name: "Robots!", subdomain: "robots")
+org2 = Organization.create(name: "Pirates", subdomain: "pirates")
+
+User.new(name: "One Eyed Eduardo", email: "OlOneEye@example.com", organization_id: org1.id)
+User.new(name: "Hooks for Hands Henry", email: "HooksForHandHenry@example.com", organization_id: org1.id)
+User.new(name: "Beep Boop", email: "beepboop@example.com", organization_id: org2.id)
+User.new(id: 4, name: "Hal", email: "HAL@example.com", organization_id: org2.id)
